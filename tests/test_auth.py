2366cf02231745df52c8c17be6587702e4c9bd50
DevChinmaysamal17 <chinmay.d.samal@gmail.com>
2026-09-01 09:11:25 +0530

    Added tests in tests/test_auth

---
+++ tests/test_auth.py
@@ -0,0 +1,48 @@
+from fastapi import Depends, HTTPException
+# from fastapi.security import OAuth2PasswordBearer
+# from sqlalchemy.orm import Session
+# from Backend import jwt_token, models
+from Backend.oauth2 import get_current_user
+from Backend.jwt_token import verify_token
+import pytest
+from unittest.mock import MagicMock, patch
+
+def test_get_current_user():
+
+  db = MagicMock()
+
+  token_data = MagicMock()
+  token_data.id = 1
+
+  user = MagicMock()
+  user.id =1
+
+  db.query.return_value.filter.return_value.first.return_value = user
+
+  with patch("Backend.oauth2.jwt_token.verify_token", return_value = token_data):
+    result = get_current_user(token="fake_token", db=db)
+
+    assert result == user
+    assert result.id == 1
+
+
+def test_get_current_user_invalid_token():
+  db = MagicMock()
+
+  credentials_exception = HTTPException(
+    status_code=401,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+  )
+
+  with patch(
+    "Backend.oauth2.jwt_token.verify_token",
+    side_effect=credentials_exception
+  ):
+    with pytest.raises(HTTPException) as exc:
+      get_current_user(
+      token="invalid_token",
+      db=db
+    )
+
+  assert exc.value.status_code == 401
